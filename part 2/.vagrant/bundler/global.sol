e3aa38d788b3f47700f3decaceea3298b83403ed
kstanis <ssk-90@tut.by>
2023-11-07 21:46:21 +0100

    add part2

---
+++ part 2/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"a699efe269336d71712a4a25a7e118a84198753dd38fef33c4d1beaeaf57a93d","vagrant_version":"2.4.0"}
